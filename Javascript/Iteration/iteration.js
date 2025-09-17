bab1499d6f3d2f86d4e25966c366f4347c1aab49
SojibIU <mdsojibhosen444@gmail.com>
2025-09-17 12:14:18 +0600

    Practiced Javascript Iteratale, Iterators, Generators

--- Javascript/Iteration/iteration.js
+++ Javascript/Iteration/iteration.js
@@ -19,16 +19,79 @@ console.log(i);
 
 
 // Complex Example: Loop Scope with var, let, and closures
-for (var i = 0; i < 3; i++) {
-  setTimeout(() => {
-    console.log("var i:", i); 
-  }, 0);
+// for (var i = 0; i < 3; i++) {
+//   setTimeout(() => {
+//     console.log("var i:", i); 
+//   }, 0);
+// }
+
+// for (var k = 0; k < 3; k++) {
+//   ((x) => {
+//     setTimeout(() => {
+//       console.log("fixed var k:", x); 
+//     }, x * 1000);
+//   })(k);
+// }
+
+
+// Create an Object
+myNumbers = {};
+
+// Make it Iterable
+myNumbers[Symbol.iterator] = function() {
+  let n = 0;
+  done = false;
+  return {
+    next() {
+      n += 10;
+      if (n == 100) {done = true}
+      return {value:n, done:done};
+    }
+  };
 }
 
-for (var k = 0; k < 3; k++) {
-  ((x) => {
-    setTimeout(() => {
-      console.log("fixed var k:", x); 
-    }, x * 1000);
-  })(k);
+// Use it
+for (let num of myNumbers) {
+  console.log(num);
+}
+
+
+function* generator() {
+  console.log("A");
+  yield 10;   // pause here
+  console.log("B"); // runs when resumed
+}
+
+
+const gen = generator();
+console.log(gen.next());
+console.log(gen.next());
+
+
+function* getRecords(records) {
+  for (let record of records) {
+    yield record; // pause and give one record at a time
+  }
+}
+
+// Example usage:
+const bigData = ["User1", "User2", "User3", "User4"]; 
+const recordStream = getRecords(bigData);
+
+console.log(recordStream.next().value); // "User1"
+console.log(recordStream.next().value); // "User2"
+// ... fetch when needed
+
+
+const playlist = {
+  songs: ["Song1", "Song2", "Song3"],
+  *[Symbol.iterator]() {
+    for (let song of this.songs) {
+      yield song;
+    }
+  }
+};
+
+for (let track of playlist) {
+  console.log(track);
 }
